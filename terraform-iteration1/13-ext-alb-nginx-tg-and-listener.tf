3b36ac22381f17d5adfb82d03d4d23c49056bd7c
onyeka-hub <onyekagodonu@yahoo.com>
2022-10-23 16:21:39 +0100

    upload

---
+++ terraform-iteration1/13-ext-alb-nginx-tg-and-listener.tf
@@ -0,0 +1,46 @@
+resource "aws_lb" "onyi-ext-alb" {
+  name               = "onyi-ext-alb"
+  internal           = false
+  ip_address_type    = "ipv4"
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.ext-alb-sg.id]
+
+  subnets = [
+    aws_subnet.public-subnet-1.id,
+    aws_subnet.public-subnet-2.id
+  ]
+
+  tags = {
+    Name = "ext-alb"
+  }
+}
+
+#--- create a target group for the external load balancer
+resource "aws_lb_target_group" "nginx-tgt" {
+  name        = "nginx-tgt"
+  port        = 80
+  protocol    = "HTTP"
+  target_type = "instance"
+  vpc_id      = aws_vpc.main.id
+
+  health_check {
+    interval            = 10
+    path                = "/healthstatus"
+    protocol            = "HTTP"
+    timeout             = 5
+    healthy_threshold   = 5
+    unhealthy_threshold = 2
+  }
+}
+
+#--- create a listener for the load balancer
+resource "aws_lb_listener" "nginx-listner" {
+  load_balancer_arn = aws_lb.onyi-ext-alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.nginx-tgt.arn
+  }
+}
